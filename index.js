e40661648f5374010de33a854e35c422b2bfee8c
Erik <erikrussells@gmail.com>
2022-06-16 16:24:06 -0400

    commit tree 41a16b6d94eb9daefc6960d610915a494d63ad12

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(c, d) {
+    return c - d;
+}
+function multiply(e, f) {
+    return e * f;
+}
+function divide(g, h) {
+    return g / h;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
